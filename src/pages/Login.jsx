feb17a3008235a39008f084ad2ec5630b07ecbc4
João Victor <joaovictordesena2020@gmail.com>
2025-08-25 01:05:31 -0300

    update Login.css and Register.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,39 @@
-import "./Authentication.css";
+import "./Login.css"
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Login = () => {
   return (
-    <div className='authentication-container'>
+    <div className='login-container'>
         <h2>Login</h2>
-        <form className='authentication-form'>
+        <form className='login-form'>
           <input type="email" placeholder='Email' required />
-          <input type="password" placeholder='Senha' />
+          <div className="password-container">
+            <input type="password" placeholder='Senha' />
+            <a href="#" className="forgot-link">Esqueceu a senha?</a>
+          </div>
           <button type='submit'>Entrar</button>
         </form>
+
+        <div className="divider">
+          <span>ou</span>
+        </div>
+
+        <div className="social-login">
+          <button className="btn-login email">
+            <MdEmail className="email-icon"/> <span>E-MAIL</span>
+          </button>
+          <button className="btn-login google">
+            <FaGoogle /> <span>GOOGLE</span>
+          </button>
+          <button className="btn-login facebook">
+            <FaFacebookF /> <span>FACEBOOK</span>
+          </button>
+        </div>
+
+        <p className="terms">
+          Ao entrar, você concorda com os nossos <a href="#">Termos</a> e <a href="#">Política de Privacidade</a>.
+        </p>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,73 @@
-import "./Authentication.css"
+import React, { useState } from "react";
+import "./Register.css"
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 const Register = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value} = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className='authentication-container'>
-        <h2>Registrar</h2>
-        <form className='authentication-form'>
-            <input type="text" placeholder='Nome' required/>
-            <input type="email" placeholder='Email' required/>
-            <input type="password" placeholder='Senha' required/>
-            <button>Registrar</button>
+    <div className="register-container">
+      <div className="register-card">
+        <h2>Bem vindo!</h2>
+        <p>Por favor selecione um provedor para criar sua conta</p>
+        
+        <div className="social-register">
+          <button className="social-btn email">
+            <MdEmail />
+          </button>
+          <button className="social-btn google">
+            <FaGoogle />
+          </button>
+          <button className="social-btn facebook">
+            <FaFacebookF />
+          </button>
+        </div>
+
+        <div className="divider">
+          <span>ou</span>
+        </div>
+
+        <form onSubmit={handleSubmit} className="register-form">
+        <div className="form-group">
+          <input type="text" name="" placeholder="nome completo" 
+          value={form.name} onChange={handleChange} required/>
+        </div>
+
+        <div className="form-group">
+          <input type="email" name="email" placeholder="E-mail" value={form.email} onChange={handleChange} required />
+        </div>
+        
+        <div className="form-group">
+          <input type="password" name="password" placeholder="criar uma senha" value={form.password} onChange={handleChange} required />
+        </div>
+
+        <button type="submit" className="register-btn">
+          Registrar
+        </button>
         </form>
+
+        <p className="redirect">
+          já possui conta? <a href="/login">Faça login</a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
